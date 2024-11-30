6d86b74ad50f8b0acce5b0bfbd8142ec2704f615
Prince Verma <vermaprince61059@gmail.com>
2024-11-30 22:18:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LudoBoard from './LudoBoard'
-import TodoList from './TodoList'
 import LotteryTicket from "./LotteryTicket.jsx";
 import TicketNum from "./TicketNum.jsx";
 import Ticket from './Ticket.jsx'
@@ -13,11 +11,7 @@ function App() {
 
   return (
     <>
-     {/* <LudoBoard /> */}
-     {/* <TodoList /> */}
      <LotteryTicket n={3} winningSum={15}/>
-     {/* <TicketNum num={5} /> */}
-
     </>
   )
 }
